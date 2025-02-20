47ebd1936420b30ef71d581bf17f61ed0fb1d2a5
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-02-20 00:25:39 +0000

    Configure webhook middleware

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,71 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface SonarRequest {
+	serverUrl: string;
+	taskId: string;
+	status: 'SUCCESS' | 'FAILED';
+	analysedAt: Date;
+	revision: string;
+	changedAt: Date;
+	project: {
+		key: string;
+		name: string;
+		url: string;
+	};
+	branch: {
+		name: string;
+		type: 'BRANCH';
+		isMain: boolean;
+		url: string;
+	};
+	qualityGate: {
+		name: string;
+		status: 'OK' | 'FAILED';
+		conditions: {
+			metric: string;
+			operator: 'LESS_THAN' | 'GREATER_THAN' | 'EQUAL';
+			value: string;
+			status: 'OK' | 'FAILED' | 'NO_VALUE';
+			errorThreshold: string;
+		}[];
+	};
+	properties: Record<string, string>;
+}
+
+interface DiscordWebhook {
+	content: string;
+	components?: object[];
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const body = await request.json<SonarRequest>();
+
+		const content = `
+		## ${body.status === 'SUCCESS' ? '✅' : '❌'} SonarQube Analysis ${body.status}
+**Project**: [${body.project.name}](https://github.com/PI-FindIt/${body.project.name})
+**Branch**: [${body.branch.name}](https://github.com/PI-FindIt/${body.project.name}/tree/${body.branch.name})
+**Quality Gate**: ${body.qualityGate.status}
+**Analysed At**: ${new Date(body.analysedAt)}
+[View Analysis](${body.project.url})
+`;
+
+		const webhook: DiscordWebhook = {
+			content,
+		};
+
+		const response = await fetch(env.DISCORD_WEBHOOK_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(webhook),
+		});
+
+		if (!response.ok) {
+			return new Response(JSON.stringify(await response.json()), { status: 500 });
+		}
+		return new Response('Discord webhook sent', { status: 200 });
 	},
 } satisfies ExportedHandler<Env>;
